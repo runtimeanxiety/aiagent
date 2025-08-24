c84a6ca4bd81286c8d965093696ca99eca03eae9
runtimeanxiety <winkie.storm@gmail.com>
2025-08-24 14:59:31 +0800

    added get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,21 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside of the permitted working directory.'
+
+    if not os.path.isfile(target_dir):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    MAX_CHARS = 10000
+    with open(target_dir, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) == MAX_CHARS:
+            file_content_string += (
+                '[...File "{file_path}" truncated at 10000 characters].'
+            )
+    return file_content_string
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,21 +3,18 @@ import os
 
 def get_files_info(working_directory, directory="."):
     files_info = ""
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
     full_path = os.path.join(working_directory, directory)
 
-    if (
-        os.path.abspath(full_path).startswith(os.path.abspath(working_directory))
-        is False
-    ):
+    if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside of the permitted working directory.'
 
-    if os.path.isdir(full_path) is False:
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory.'
 
     try:
-        for file_name in os.listdir(full_path):
+        for file_name in os.listdir(target_dir):
             files_info += f"- {file_name}: file_size={os.path.getsize(os.path.join(full_path, file_name))} bytes, is_dir={os.path.isdir(os.path.join(full_path, file_name))}\n"
     except Exception as e:
         return f"Error: {e}"
-
-    return files_info
