80efae8f6c7e12fb5304d5a1dd03c5f61ba8b20b
runtimeanxiety <winkie.storm@gmail.com>
2025-08-24 12:58:13 +0800

    added the base code and calculator

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+user_prompt = sys.argv[1]
+
+if len(sys.argv) < 2:
+    print("Usage: python main.py '<your prompt here>'")
+    sys.exit(1)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+)
+
+if sys.argv[2] == "--verbose":
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    None
